24c774f4760f02d39d358b597e17ad31738d44ca
hange1006 <69919655+hange1006@users.noreply.github.com>
2020-08-19 14:11:13 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Emily");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1");
+		System.out.println("August 19");
+		System.out.println("CMSC 255 Section 002");
+		System.out.println();
+
+	}
+}
